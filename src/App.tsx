5e6479b7469581dba87e46e398a37af542b7da3d
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2023-11-19 15:42:51 +0900

    text 수정

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>^ㅡ^, Jung EunWoo</h1>
+      <h1>^ㅡ^, Jung EunWoo, I love mintchocolate</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
